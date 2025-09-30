05266f11cb41c9986f56b012ed2eafbd9fc14cf3
MohamedAli <mmkandeelz74@gmail.com>
2025-09-30 07:15:51 +0300

    feat(about): implement about section with responsive design, dark mode, and Arabic translation

--- src/app/[locale]/(homepage)/_components/about-image.tsx
+++ src/app/[locale]/(homepage)/_components/about-image.tsx
@@ -2,15 +2,15 @@ import Image from "next/image";
 
 export default function AboutImage() {
   return (
-    <div className="relative flex items-start gap-2 mt-5">
+    <div className="relative flex items-start gap-2 mt-5 col-span-1">
       <div className="relative">
-        <div className="w-64 h-85 absolute -top-5 -left-5 -rotate-custom-1 rounded-[120px] border-4 border-maroon-600 rounded-tl-[50px] object-cover" />
+        <div className="w-80 h-86 absolute -top-5 -left-5 -rotate-custom-1 rounded-[120px] border-4 border-maroon-600 rounded-tl-[50px] dark:border-soft-pink-200 object-cover" />
         <Image
           src="/assets/Images/about-1.png"
           alt="about image"
           width={300}
           height={0}
-          className="w-76 h-84 left-7 top-5 rotate-0 rounded-[120px] rounded-tl-[50px] object-cover"
+          className="w-[450px] h-86 left-7 top-5 rotate-0 rounded-[120px] rounded-tl-[50px] object-cover"
         />
       </div>
       <div className="h-84 flex flex-col gap-2">
@@ -19,14 +19,14 @@ export default function AboutImage() {
           alt="about image"
           width={300}
           height={0}
-          className="size-48 rounded-full object-cover"
+          className="size-52 rounded-full object-cover"
         />
         <Image
           src="/assets/Images/about-2.png"
           alt="about image"
           width={300}
           height={0}
-          className="w-48 h-36 rounded-s-[50px] rounded-e-[100px] object-cover"
+          className="w-[200px] h-36 rounded-s-[50px] rounded-e-[100px] object-cover"
         />
       </div>
     </div>
--- src/app/[locale]/(homepage)/_components/about-us-section.tsx
+++ src/app/[locale]/(homepage)/_components/about-us-section.tsx
@@ -1,50 +1,53 @@
 import { ArrowRight, Check } from "lucide-react";
+import { useTranslations } from "next-intl";
 import { Button } from "@/components/ui/button";
 import AboutImage from "./about-image";
 
 export default function AboutUs() {
+  // Translation
+  const t = useTranslations();
   return (
-    <section className="max-w-screen-2xl my-14 px-4 sm:px-6 md:px-12 lg:px-20 mx-auto flex items-center gap-12 xl:gap-20">
+    <section className="my-36 px-4 lg:px-20 grid grid-cols-1 lg:grid-cols-2  items-center justify-center">
       <AboutImage />
-      <div className="max-w-xl">
-        <h3 className="font-bold leading-8 tracking-title align-middle uppercase text-soft-pink-500">
-          About
+      <div className="col-span-1">
+        <h3 className="font-bold leading-8 tracking-title align-middle uppercase text-soft-pink-500 dark:text-maroon-400">
+          {t("about")}
         </h3>
-        <p className="mt-6 text-maroon-700 font-bold text-3xl ">
-          Delivering the <span className="text-soft-pink-500">Finest</span> Gift Boxes for Your{" "}
-          <span className="text-soft-pink-500">Special</span> Moments
+        <p className="mt-6 text-maroon-700 dark:text-soft-pink-200 font-bold text-3xl">
+          {t.rich("about-section.title", {
+            pink: (chunk) => (
+              <span className="text-soft-pink-500 dark:text-maroon-400">{chunk}</span>
+            ),
+          })}
         </p>
-        <p className="font-Sarabun font-normal text-zinc-500 mt-2">
-          Make every moment memorable with our premium gift boxes. Carefully curated and beautifully
-          packaged, each box is filled with handpicked items designed to impress. Whether it&#39;s
-          for a birthday, wedding, or a simple “thank you,” our gift boxes are crafted to leave a
-          lasting impression — because thoughtful gifting starts here.
+        <p className="font-Sarabun font-normal text-zinc-500 dark:text-zinc-400 mt-2">
+          {t("about-description")}
         </p>
         <Button className="bg-maroon-600 h-10 w-32 mt-6 hover:bg-maroon-600 py-3 px-5 rounded-lg font-Sarabun">
-          Discover
+          {t("discover")}
           <span>
-            <ArrowRight className="ml-1.5" size={16} />
+            <ArrowRight className="ml-1.5 rtl:rotate-180" size={16} />
           </span>
         </Button>
         <div className="mt-10 flex items-start gap-6">
           <div className="flex flex-col gap-5 items-start pl-2.5">
-            <div className=" font-Sarabun text-zinc-800 flex items-center gap-5">
-              <Check />
-              Competitive Prices & Easy Shopping
+            <div className=" font-Sarabun text-zinc-800 flex items-center gap-5 dark:text-zinc-50">
+              <Check className="text-maroon-600 dark:text-soft-pink-200" />
+              {t("competitive-prices-and-easy-shopping")}
             </div>
-            <div className=" font-Sarabun text-zinc-800 flex items-center gap-5">
-              <Check />
-              Perfect for Every Occasion
+            <div className=" font-Sarabun text-zinc-800 flex items-center gap-5 dark:text-zinc-50">
+              <Check className="text-maroon-600 dark:text-soft-pink-200" />
+              {t("perfect-for-every-occasion")}
             </div>
           </div>
           <div className="flex flex-col gap-5 items-start pl-2.5 ">
-            <div className=" font-Sarabun text-zinc-800 flex items-center gap-5">
-              <Check />
-              Premium Quality & Elegant Packaging
+            <div className=" font-Sarabun text-zinc-800 flex items-center gap-5 dark:text-zinc-50">
+              <Check className="text-maroon-600 dark:text-soft-pink-200" />
+              {t("premium-quality-and-elegant-packaging")}
             </div>
-            <div className=" font-Sarabun text-zinc-800 flex items-center gap-5">
-              <Check />
-              Fast & Reliable Delivery
+            <div className=" font-Sarabun text-zinc-800 flex items-center gap-5 dark:text-zinc-50">
+              <Check className="text-maroon-600 dark:text-soft-pink-200" />
+              {t("fast-and-reliable-delivery")}
             </div>
           </div>
         </div>
