36d9c7ea5c24ec8a4c72f2088d2d92ed1883c38b
MohamedAli <mmkandeelz74@gmail.com>
2025-10-01 09:40:02 +0300

    feat(i18n): add dark mode and translations for compnies section

--- src/app/[locale]/(homepage)/_components/companies-section.tsx
+++ src/app/[locale]/(homepage)/_components/companies-section.tsx
@@ -1,20 +1,60 @@
 import Image from "next/image";
+import { useTranslations } from "next-intl";
 
 export default function CompaniesSection() {
+  // Translations
+  const t = useTranslations();
   return (
-    <section>
-      <div className="bg-maroon-50 dark:bg-zinc-700 rounded-[20px] w-full min-h-56 px-6 flex flex-col items-center justify-center">
-        <h2 className="font-bold font-Sarabun text-maroon-700 text-4xl">
-          Trusted by over<span className="text-soft-pink-500"> 4.5k+ </span>
-          companies
+    <section className="mt-36 mb-80 lg:px-20 px-4">
+      <div className="bg-maroon-50 dark:bg-zinc-700 rounded-[20px] w-full py-10 px-6 flex flex-col items-center justify-center">
+        <h2 className="font-bold font-Sarabun text-maroon-700 text-4xl dark:text-soft-pink-200 text-center">
+          {t.rich("stats.trusted", {
+            pink: (chunk: string) => <span className="text-soft-pink-500">{chunk}</span>,
+          })}
         </h2>
-        <div className="flex items-center justify-between flex-wrap w-full mt-10">
-          <Image src={"/assets/Images/companie-1.png"} alt="companie" width={150} height={30} />
-          <Image src={"/assets/Images/companie-2.png"} alt="companie" width={150} height={30} />
-          <Image src={"/assets/Images/companie-3.png"} alt="companie" width={150} height={30} />
-          <Image src={"/assets/Images/companie-4.png"} alt="companie" width={150} height={30} />
-          <Image src={"/assets/Images/companie-5.png"} alt="companie" width={150} height={30} />
-          <Image src={"/assets/Images/companie-6.png"} alt="companie" width={150} height={30} />
+        <div className="grid lg:grid-cols-6 md:grid-cols-3 grid-cols-1 items-center justify-center mt-10 w-full">
+          <Image
+            src={"/assets/Images/companie-1.png"}
+            alt="company"
+            className="col-span-1 mx-auto"
+            width={150}
+            height={30}
+          />
+          <Image
+            src={"/assets/Images/companie-2.png"}
+            alt="company"
+            className="col-span-1 mx-auto"
+            width={150}
+            height={30}
+          />
+          <Image
+            src={"/assets/Images/companie-3.png"}
+            alt="company"
+            className="col-span-1 mx-auto"
+            width={150}
+            height={30}
+          />
+          <Image
+            src={"/assets/Images/companie-4.png"}
+            alt="company"
+            className="col-span-1 mx-auto"
+            width={150}
+            height={30}
+          />
+          <Image
+            src={"/assets/Images/companie-5.png"}
+            alt="company"
+            className="col-span-1 mx-auto"
+            width={150}
+            height={30}
+          />
+          <Image
+            src={"/assets/Images/companie-6.png"}
+            alt="company"
+            className="col-span-1 mx-auto"
+            width={150}
+            height={30}
+          />
         </div>
       </div>
     </section>
--- src/app/[locale]/(homepage)/_components/testimonials-section/testimonials-section.tsx
+++ src/app/[locale]/(homepage)/_components/testimonials-section/testimonials-section.tsx
@@ -5,12 +5,10 @@ const TestimonialsSection = () => {
   // Translations
   const t = useTranslations();
   return (
-    <section>
+    <section className="text-soft-pink-500 dark:text-maroon-400">
       {/* Heading */}
       <div className="my-10 text-center">
-        <h2 className="text-base font-bold text-soft-pink-500 dark:text-maroon-400">
-          {t("testimonials")}
-        </h2>
+        <h2 className="text-base font-bold">{t("testimonials")}</h2>
         <h3 className="relative mx-auto w-fit text-4xl font-bold text-maroon-700 before:absolute before:bottom-0 before:h-[1px] before:w-[30%] before:bg-maroon-400 after:absolute after:bottom-0 after:left-0 after:-z-10 after:h-1/2 after:w-[70%] after:rounded-e-full after:bg-maroon-100 dark:text-soft-pink-200 before:dark:bg-maroon-200 after:dark:bg-zinc-700 after:rtl:right-0">
           {t("real-words-from-happy-customers")}
         </h3>
