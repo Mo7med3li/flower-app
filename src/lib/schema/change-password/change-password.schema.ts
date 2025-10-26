9cf43f7f389e6fce768ab8f5d2524db81db8e2d5
MohamedAli <mmkandeelz74@gmail.com>
2025-10-27 00:14:53 +0300

    refactor: add some instructure comments

--- src/lib/schema/change-password/change-password.schema.ts
+++ src/lib/schema/change-password/change-password.schema.ts
@@ -2,10 +2,6 @@ import z from "zod";
 
 export const changePasswordSchema = z.object({
   password: z.string({ error: "Password is required" }).min(1, "Password is required"),
-  //   .regex(
-  //     /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{9,}$/,
-  //     "Password must be at least 8 characters include at least 1 upper, 1 lower, and number.",
-  //   ),
   newPassword: z
     .string({ error: "Password is required" })
     .min(1, "Password is required")
@@ -14,8 +10,5 @@ export const changePasswordSchema = z.object({
       "Password must be at least 8 characters include at least 1 upper, 1 lower, and number.",
     ),
 });
-//   .refine((values) => values.password === values.newPassword, {
-//     message: "password do not match",
-//     path: ["newPassword"],
-//   });
+// type
 export type ChangePasswordFormType = z.infer<typeof changePasswordSchema>;
