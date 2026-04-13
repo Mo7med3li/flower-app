2a4db43726bbf626bf91064f6b9fc365d1b5fd08
Mohamed Ali <30309101700696@ci.menofia.edu.eg>
2026-04-14 01:25:58 +0200

    fix

--- src/lib/api/notifications.ts
+++ src/lib/api/notifications.ts
@@ -1,7 +1,6 @@
 export async function fetchUserNotification({ pageParam }: { pageParam: number }) {
   const response = await fetch(
-    `${process.env.NEXT_PUBLIC_API}/user-notifications?page=${pageParam}`,
-    { credentials: "include" },
+    `/api/user-notifications?page=${pageParam}`,
   );
 
   const payload = await response.json();
