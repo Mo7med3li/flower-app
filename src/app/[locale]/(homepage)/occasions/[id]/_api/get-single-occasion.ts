f7105bc2ff0ec3e8c4aa61933f016d0489c55bb0
Mohamed Ali <30309101700696@ci.menofia.edu.eg>
2026-05-01 03:26:16 +0300

    fix single occasion api

--- src/app/[locale]/(homepage)/occasions/[id]/_api/get-single-occasion.ts
+++ src/app/[locale]/(homepage)/occasions/[id]/_api/get-single-occasion.ts
@@ -1,5 +1,9 @@
 export async function getSingleOccasion(id: string) {
-  const response = await fetch(`/api/occasion/${id}`);
+  const response = await fetch(`${process.env.API}/occasions/${id}`);
+
   const payload = await response.json();
+  if (!payload.status) {
+    throw new Error("Failed to fetch occasion");
+  }
   return payload;
 }
